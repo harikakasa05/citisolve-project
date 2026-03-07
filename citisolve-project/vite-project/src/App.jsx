cf313efdc60d222599c3c1123aec56eb84c0f278
HARIKA KASA <harikakasa05@gmail.com>
2026-03-07 19:48:37 +0530

    Update App.jsx

--- citisolve-project/vite-project/src/App.jsx
+++ citisolve-project/vite-project/src/App.jsx
@@ -1,26 +1,59 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ComplaintForm from "./Components/ComplaintForm";
+import MyComplaint from "./Components/MyComplaint";
+import { useAuth } from "./Context/AuthContext";
 
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import ComplaintForm from './Components/ComplaintForm'
-import MyComplaint from './Components/MyComplaint'
+// Protected Route
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
 
 const App = () => {
   return (
     <>
       <Navbar />
+
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/complaintform' element={<ComplaintForm />} />
-        <Route path='/my-complaint' element={<MyComplaint />} />
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/complaintform"
+          element={
+            <ProtectedRoute>
+              <ComplaintForm />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/my-complaint"
+          element={
+            <ProtectedRoute>
+              <MyComplaint />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* fallback route for deployment */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
