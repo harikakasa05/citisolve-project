4904efc65e56dab62d7895aefd7087a3b6451555
HARIKA KASA <harikakasa05@gmail.com>
2026-03-07 14:02:32 +0530

    Update server.js

--- citisolve-project/backend/server.js
+++ citisolve-project/backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://citisolve-fullstack-project-1.onrender.com" 
+    "https://citisolve-frontend.onrender.com" 
 ];
 
 app.use(
