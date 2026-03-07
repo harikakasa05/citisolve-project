561f2b9c34a946bbd3d638aa06ea56f5fa1afad9
HARIKA KASA <harikakasa05@gmail.com>
2026-03-07 18:55:58 +0530

    Update ComplaintForm.jsx

--- citisolve-project/vite-project/src/Components/ComplaintForm.jsx
+++ citisolve-project/vite-project/src/Components/ComplaintForm.jsx
@@ -63,7 +63,7 @@ const ComplaintForm = () => {
             }
 
             alert("Complaint Submitted Successfully!");
-            navigate("/my-complaint");
+            navigate("/my-complaints");
         } catch (error) {
             console.error("Error:", error);
             alert(error.message || "Something went wrong!");
@@ -132,7 +132,7 @@ const ComplaintForm = () => {
                 <div className="button-group">
                     <button
                         type="button"
-                        onClick={() => navigate("/my-complaint")}
+                        onClick={() => navigate("/my-complaints")}
                     >
                         Cancel
                     </button>
