34e0a95c5400017414abdb5990f3956146161996
HARIKA KASA <harikakasa05@gmail.com>
2026-03-07 19:19:24 +0530

    Update AuthContext.jsx

--- citisolve-project/vite-project/src/Context/AuthContext.jsx
+++ citisolve-project/vite-project/src/Context/AuthContext.jsx
@@ -6,13 +6,6 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
-        useEffect(() => {
-        const storedUser = localStorage.getItem("user");
-        if (storedUser) {
-            setUser(JSON.parse(storedUser));
-        }
-    }, []);
-
     const login = (userData) => {
         setUser(userData);
         localStorage.setItem("user", JSON.stringify(userData));
